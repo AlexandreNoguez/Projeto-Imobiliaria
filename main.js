68968affdc4cba6ab7362f109db1148af3c14049
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2022-04-02 13:54:42 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+// const nav = document.querySelector('#header nav')
+// const toggle = document.querySelectorAll('nav toggle')
+
+// for (const element of toggle){
+//     element.addEventListener('click', function(){
+//         nav.classList.toggle('show')
+//     })
+// }
+
+// const links = document.querySelectorAll('nav ul li a')
+
+// for(const link = links){
+//     link.addEventListener('click', function(){
+//         nav.classList.remove('show')
+//     })
+// }
+
+
+// SWIPER HOME
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+// WHATSAPP BUTTON
+
